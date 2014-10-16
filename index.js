b120a190f6506836cef2a8007701acb920cc8491
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-16 11:30:32 -0700

    dont reuse incoming hash bump to 1.0.1

--- index.js
+++ index.js
@@ -5,22 +5,23 @@ module.exports = {
     var host = options.host || env.DOCKER_IP
     var port = options.port || env.DOCKER_PORT
     var full_host = options.docker_host || env.DOCKER_HOST
+    var out = {};
     if (host && port) {
-      options.host = host
-      options.port = port
+      out.host = host
+      out.port = port
     } else if (full_host) {
       var uri = URI.parse(full_host);
       if (uri.scheme === "http" || uri.scheme === "tcp") {
-        options.host = uri.host
-        options.port = uri.port
+        out.host = uri.host
+        out.port = uri.port
       } else if (uri.scheme === "unix" || uri.path) {
-        options.socketPath = uri.path
+        out.socketPath = uri.path
       } else {
         throw new Error("Could not normalize options from DOCKER_HOST")
       }
     } else {
-      options.socketPath = '/var/run/docker.sock'
+      out.socketPath = '/var/run/docker.sock'
     }
-    return options;
+    return out;
   }
 }
